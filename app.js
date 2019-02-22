d2a627c5b611382f8d08753ec65ebfc8fb8d5428
unknown <GPriporski@GPriporski-PC.int.sofia.bg>
2019-02-22 11:09:00 +0200

    err fix

--- app.js
+++ app.js
@@ -8,21 +8,15 @@ require('./config/passport')(passport)
 // Load Routes
 const auth = require('./routes/auth')
 
-const app = express()
-
-// ======================================== ROUTES ================================ //
-
-// Home Route
+const app = express();
 app.get('/', (req, res) => {
-  res.send('It works')
+  res.send('It Works!')
 })
-
-// ===================================== END ROUTES =============================== //
 
 // Use Routes
 app.use('/auth', auth)
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
