986061f50ec2bc84878268a75b000f5265ac7b69
unknown <GPriporski@GPriporski-PC.int.sofia.bg>
2019-02-22 11:03:42 +0200

    err fix

--- config/keys.js
+++ config/keys.js
@@ -3,5 +3,6 @@ module.exports = {
     "mongodb://admin:admin1234@ds147225.mlab.com:47225/adventurememos-dev",
   googleClientID:
     "644980611573-3vjkgbmctit1hkrvl030quga48g7250m.apps.googleusercontent.com",
-  googleClientSecret: "h4hzCSCklW6S7EkWg9oaB085"
+  googleClientSecret:
+    "h4hzCSCklW6S7EkWg9oaB085"
 };
