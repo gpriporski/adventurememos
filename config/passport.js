2751b17a8d5a18c10017cb6747cba0ce96a90534
gpriporski <priporski@gmail.com>
2019-02-23 10:41:55 +0200

    navbar done

--- config/passport.js
+++ config/passport.js
@@ -6,39 +6,38 @@ const User = mongoose.model('users')
 
 module.exports = function (passport) {
   passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback',
-        proxy: true
-      },
-      (accessToken, refreshToken, profile, done) => {
-        const image = profile.photos[0].value.substring(
-          0,
-          profile.photos[0].value.indexOf('?')
-        )
-        const newUser = {
-          googleID: profile.id,
-          firstName: profile.name.givenName,
-          lastName: profile.name.familyName,
-          email: profile.emails[0].value,
-          image: image
-        }
-
-        // Check for existing user
-        User.findOne({
-          googleID: profile.id
-        }).then(user => {
-          if (user) {
-            // Return User
-            done(null, user)
-          } else {
-            // Create User
-            new User(newUser).save().then(user => done(null, user))
-          }
-        })
+    new GoogleStrategy({
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
+    (accessToken, refreshToken, profile, done) => {
+      const image = profile.photos[0].value.substring(
+        0,
+        profile.photos[0].value.indexOf('?')
+      )
+      const newUser = {
+        googleID: profile.id,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        email: profile.emails[0].value,
+        image: image
       }
+
+      // Check for existing user
+      User.findOne({
+        googleID: profile.id
+      }).then(user => {
+        if (user) {
+          // Return User
+          done(null, user)
+        } else {
+          // Create User
+          new User(newUser).save().then(user => done(null, user))
+        }
+      })
+    }
     )
   )
   passport.serializeUser((user, done) => {
@@ -48,4 +47,4 @@ module.exports = function (passport) {
   passport.deserializeUser((id, done) => {
     User.findById(id).then(user => done(null, user))
   })
-};
+}
